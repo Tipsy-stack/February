b4fc8854905745aad5c44b281829b0c0ba8398ee
Olatipe <osatuyiolatipe@gmail.com>
2025-07-22 12:38:45 +0100

    added src

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, labore!</p>
+    </div>
+  );
+}
+export default App;
